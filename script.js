c01fad372717ea84cde8023c3b074308965e315b
gracechawasichali <gracesichali2@gmail.com>
2022-04-22 18:00:44 +0100

    add get in touch details

--- script.js
+++ script.js
@@ -6,10 +6,34 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
 
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+     //scroll up script
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop:0});
+     });
+
     //toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
+    //typing menu/navbar script
+    var typed = new Typed(".typing", {
+        strings: ["Developer","Designer","Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+    var typed = new Typed(".typing-2" , {
+        strings: ["Developer","Designer","Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
 });
